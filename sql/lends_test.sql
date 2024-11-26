09cdf9ba7f26f6b81ccbe7204a41f3ee2014bfa0
Honoka Guo <guoyuan.p@foxmail.com>
2024-11-26 21:45:45 +0800

    为lends添加了少量测试用例

---
+++ sql/lends_test.sql
@@ -0,0 +1,3 @@
+INSERT INTO `lib_sys`.`lends` (`id`, `user_id`, `book_id`, `lend_time`, `return_time`, `returned`) VALUES ('1', '3', '1', '2024-11-26 20:05:27', '2024-11-27 20:05:30', '0');
+INSERT INTO `lib_sys`.`lends` (`id`, `user_id`, `book_id`, `lend_time`, `return_time`, `returned`) VALUES ('2', '3', '2', '2024-11-06 20:05:51', '2024-11-21 20:05:54', '0');
+INSERT INTO `lib_sys`.`lends` (`id`, `user_id`, `book_id`, `lend_time`, `return_time`, `returned`) VALUES ('3', '2', '3', '2024-11-20 20:06:05', '2024-11-29 20:06:08', '0');
