69a41b6e10cce04251dac8e57ac92a811b12beb7
Honoka Guo <guoyuan.p@foxmail.com>
2024-11-26 15:50:24 +0800

    完善了bookDTO

--- src/main/java/com/example/backend/entity/BookInfoDTO.java
+++ src/main/java/com/example/backend/entity/BookInfoDTO.java
@@ -1,6 +1,36 @@
 package com.example.backend.entity;
 
-
 //The result of linking bookEntity with bookIsbn
+//DTO stands for Data Transfer Object
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class BookInfoDTO {
+    private int id;
+    private String isbn;
+    private String status;
+    private String place;
+
+    private String title;
+    private float price;
+    private String author;
+    private String publisher;
+    private String borrownum;
+
+    public BookInfoDTO(int id, String isbn, String status, String place, String title,
+                       float price, String author, String publisher, String borrownum) {
+        this.id = id;
+        this.isbn = isbn;
+        this.status = status;
+        this.place = place;
+        this.title = title;
+        this.price = price;
+        this.author = author;
+        this.publisher = publisher;
+        this.borrownum = borrownum;
+    }
 }
