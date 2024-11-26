f3e15b329db3637d5ad198ca2a3af634140f27c1
And_Ocean <and_ocean@163.com>
2024-11-26 15:42:59 +0800

    update book entity

--- src/main/java/com/example/backend/entity/BookEntity.java
+++ src/main/java/com/example/backend/entity/BookEntity.java
@@ -1,4 +1,23 @@
 package com.example.backend.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@TableName("bookentity")
 public class BookEntity {
+    private int id;
+    private String isbn;
+    private String status;
+    private String place;
+
+    public BookEntity(int id, String isbn, String status, String place){
+        this.id = id;
+        this.isbn = isbn;
+        this.status = status;
+        this.place = place;
+    }
 }
--- src/main/java/com/example/backend/entity/BookISBN.java
+++ src/main/java/com/example/backend/entity/BookISBN.java
@@ -1,4 +1,28 @@
 package com.example.backend.entity;
 
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+@TableName("bookisbn")
 public class BookISBN {
+    private String isbn;
+    private String title;
+    private float price;
+    private String author;
+    private String publisher;
+    private String borrownum;
+
+    public BookISBN(String isbn,String title, float price, String author,
+                          String publisher, String borrownum) {
+        this.isbn = isbn;
+        this.title = title;
+        this.price = price;
+        this.author = author;
+        this.publisher = publisher;
+        this.borrownum = borrownum;
+    }
 }
